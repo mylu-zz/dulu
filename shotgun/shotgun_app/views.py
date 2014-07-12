15c6be91648b277a30c7af08eeca61d8d419030c
Max Lu <malu@malu-mn1.linkedin.biz>
2014-07-11 23:06:31 -0700

    newsfeed fuck

--- shotgun/shotgun_app/views.py
+++ shotgun/shotgun_app/views.py
@@ -8,5 +8,9 @@ from shotgun_app.models import Users,Events,Attending, Friends
 
 def index(request):
     latest_event_list = Events.objects.all().order_by('-event_date')[:15]
-    context = {'latest_event_list': latest_event_list}
+    user = Users.objects.get(pk=0)
+    context = {
+    	'latest_event_list': latest_event_list,
+    	'user': user
+    }
     return render(request, 'shotgun_app/index.html', context)
